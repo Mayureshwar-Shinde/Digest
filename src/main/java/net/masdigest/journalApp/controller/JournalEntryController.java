86ead19d9ad5e28674d801b69a44b8910e0db04b
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2025-10-20 15:46:34 +0530

    commit 0 : intial commit

---
+++ src/main/java/net/masdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,24 @@
+package net.masdigest.journalApp.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.masdigest.journalApp.entity.JournalEntry;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+	
+	
+}
