8a66ec7d934bbfec17feb3699cf8ad4f7abae802
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2025-10-20 15:48:38 +0530

    commit 1 : added basic crud for journal records

--- src/main/java/net/masdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/masdigest/journalApp/controller/JournalEntryController.java
@@ -20,5 +20,33 @@ import net.masdigest.journalApp.entity.JournalEntry;
 @RequestMapping("/journal")
 public class JournalEntryController {
 	
+	private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 	
+	@GetMapping
+	public List<JournalEntry> getAll() {
+		return new ArrayList<>(journalEntries.values());
+	}
+	
+	@PostMapping
+	public String createEntry(@RequestBody JournalEntry journalEntry) {
+		journalEntries.put(journalEntry.getId(), journalEntry);
+		return journalEntry.getTitle();
+	}
+	
+	@GetMapping("/{id}")
+	public JournalEntry findById(@PathVariable Long id) {
+		return journalEntries.get(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public boolean deleteJournalEntry(@PathVariable Long id) {
+		journalEntries.remove(id);
+		return true;
+	}
+	
+	@PutMapping("/{id}")
+	public JournalEntry updateJournalEntry(@RequestBody JournalEntry journalEntry, @PathVariable Long id) {
+		journalEntries.put(id, journalEntry);
+		return journalEntries.get(id);
+	}
 }
