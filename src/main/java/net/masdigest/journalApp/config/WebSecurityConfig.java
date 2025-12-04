a0f3c68e7d30bf35cabeec65b240fcb757e892c2
Mayureshwar Shinde <mayureshwarshinde@gmail.com>
2025-12-04 22:02:28 +0530

    commit 5 : added basic demo rbac through configuration

--- src/main/java/net/masdigest/journalApp/config/WebSecurityConfig.java
+++ src/main/java/net/masdigest/journalApp/config/WebSecurityConfig.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+	
+	private final PasswordEncoder passwordEncoder;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -21,6 +31,21 @@ public class WebSecurityConfig {
 			)
 			.formLogin(Customizer.withDefaults());
 		return httpSecurity.build();
+	}
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails user1 = User.withUsername("admin")
+				.password(passwordEncoder.encode("password"))
+				.roles("ADMIN")
+				.build();
+		
+		UserDetails user2 = User.withUsername("patient")
+				.password(passwordEncoder.encode("password"))
+				.roles("TESTER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user1, user2);
 	}
 
 }
